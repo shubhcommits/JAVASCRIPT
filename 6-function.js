7a05c40aa1b1e20c08a82955b70fd1154569e0d8
Shubham <shm.code1@gmail.com>
2023-08-30 15:50:18 +0530

    added some files

---
+++ 6-function.js
@@ -0,0 +1,14 @@
+function getMilk(){
+    console.log("leave house");
+    console.log("move right");
+    console.log("move left");
+}
+getMilk();
+/**********************************************************************/
+// in this function can be given an input
+function getMilk(bottles){
+    console.log("leave house"+bottles);
+    console.log("move right");
+    console.log("move left");
+}      
+getMilk(5);
---
+++ 9-Math_random.js
@@ -0,0 +1,6 @@
+// Math.random();
+// here we created a dice roll
+var n;
+n=Math.random();  // this will give an output between 0 & 0.99999...
+var round=Math.floor(n*6)  // extract integer part
+console.log(round);
