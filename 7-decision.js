44dce9dbc6fcc5c50408c7a67881a02bb0168d57
Shubham <shm.code1@gmail.com>
2023-09-04 22:17:32 +0530

    decision js if/else

---
+++ 7-decision.js
@@ -0,0 +1,19 @@
+// if else in js 
+const age=15;
+if(age>=18){
+    console.log(`Eligible for driving`);
+}
+else{
+    console.log(`${18-age} years are  left for driving `);
+}
+
+// Another example
+const birthYear=2004;
+let centuary;
+if(birthYear<2000){
+    centuary=20;
+}
+else{
+    centuary=21;
+}
+console.log(`${centuary}`);
