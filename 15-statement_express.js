829aac0b2f7f92bf3705b048c35ad921f2bf8741
Shubham <shm.code1@gmail.com>
2023-09-05 03:08:43 +0530

    difference between statement and expression

---
+++ 15-statement_express.js
@@ -0,0 +1,14 @@
+1+5  // this is an expression
+1994  // expression
+true&&false // expression
+if(25>10){  // this is statement
+    const str="25 is biggger";
+}
+const me = "Shubham";  // Here Shubham is string which is assign to me so it is expression;
+console.log(`I'm ${2023-2004} years old ${me}`);  // Inside {} only expression can be written
+
+/*Expressions can be assigned or used as operands, while statements can only be declared.
+
+Statements create side effects to be useful, while expressions are values or execute to values.
+
+Expressions are unique in meaning, while statements are two-sided in execution. For example, 1 has a certain value while go( ) may be executed or not.*/
