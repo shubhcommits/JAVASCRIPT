3f34636d7d44abac441807ef5219c12439faf060
Shubham <shm.code1@gmail.com>
2023-09-05 03:02:31 +0530

    difference between (==) and (===) (!==) (!=)

---
+++ 10-equalityOperator.js
@@ -0,0 +1,9 @@
+// const age=18;
+const age="18";  // = this is assignment here
+if(age===18){  /// === this also check type
+    // try always use ===
+    console.log("You became an adult :D (strict)")
+}
+if(age==18){  // this check only value not type
+    console.log("You became adult :D (loose)")
+}
