6f31d1c67a184bfaf0b125626e3894372764a951
Shubham <shm.code1@gmail.com>
2023-09-04 21:09:13 +0530

    operators in js

---
+++ 4-operators.js
@@ -0,0 +1,22 @@
+//Math operators
+const now=2023;
+const ageJohn=now-2002;
+const ageSena=now-2003;
+console.log(ageJohn,ageSena);
+console.log(ageJohn*2,ageJohn/3,2 ** 3);
+// here 2 ** 3 means 2 to the power 3
+var firstName="Shubham";
+var lastName="Maurya";
+console.log(firstName+" "+lastName); // here concanating two varibles
+// assignment operators
+let x=10+5;  
+// x++
+// x--;
+// x+=10;
+
+// comparison operators
+console.log(ageJohn>ageSena);  // they are just boolean operators 
+// we have > , < , <= , >=;
+console.log(ageJohn===ageSena);
+const avgAge=(ageJohn+ageSena)/2;
+console.log(avgAge);
