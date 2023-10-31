95b2d1fd7501f306993dac207a49dc3bd9458c8e
Shubham <shm.code1@gmail.com>
2023-10-31 22:49:35 +0530

    ds and modern operators

--- 4-Dice-game/script.js
+++ 4-Dice-game/script.js
@@ -69,7 +69,7 @@ btnhold.addEventListener('click',function(){
     scores[activePlayer]=scores[activePlayer]+currentScore;
     document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
 
-    if(scores[activePlayer]>=100){
+    if(scores[activePlayer]>=20){
         playing=false;
         diceEl.classList.add('hidden');
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
--- 5-DS_and_OPERATORS/9-opticalChaining.js
+++ 5-DS_and_OPERATORS/9-opticalChaining.js
@@ -34,3 +34,43 @@ const restaurant = {
 };
 
 console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days=['mon','tue','wed','thu','fri','sat','sun'];
+for(const day of days){
+    const open=restaurant.openingHours[day]?.open ?? "closed";
+    // nullish value are null and undefined
+    console.log(`On ${day} we are open at ${open}`)
+}
+
+// Methods - optional chaning work on functions
+console.log(restaurant?.order?.(0,1)?? "Method does not exist");
+
+// console.log(restaurant.orderPasta?.(1,2,3) ?? "Method does no exist");
+
+// Optional chaining in arrays
+const users=[{
+    name: 'Shubham',
+    email: 'shm.code1@gmail.com',
+}];
+console.log(users[0]?.name??"user array empty");
+
+// Looping objects
+// properties names
+const properties=Object.keys(restaurant.openingHours);
+console.log(properties);
+let openStr=`We are open on ${properties.length} days:`; 
+for(const day of properties){
+    openStr += `${day}, `;
+}
+console.log(openStr);
+
+// properties values
+const values=Object.values(restaurant.openingHours);
+console.log(values);
+
+// Enteries object
+const entries=Object.entries(restaurant.openingHours);
+for(const [day, {open,close}] of entries){
+    console.log(`On day ${day} we are open at ${open} and close at ${close}`)
+}
